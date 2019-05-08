2f5bd9e17ba5e631eb89e8fe6485bf600d571f61
sistemas <sistemas@LAB3C12.fclf.lab>
2019-05-07 21:19:49 -0300

    primeiro commit

---
+++ SpringAula/src/br/edu/aula/controller/OlaMundoController.java
@@ -0,0 +1,14 @@
+package br.edu.aula.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OlaMundoController {
+	
+		@RequestMapping("/olaMundoSpring")
+	    public String execute() {
+	        System.out.println("Ola Mundo Spring");
+	        return "ok";
+	    }	
+}
